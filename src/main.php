c690fd6d1c07ffaedbb9f88c5c4f890813e67600
James Smith <smith7@uleth.ca>
2021-11-11 16:55:46 -0700

    actually edited main.php and properties.css.

--- src/main.php
+++ src/main.php
@@ -9,7 +9,10 @@
 	<h1>Events Database</h1>
 	<p>Please select one of the following operations:</p>
 	<ul>
-		<li></li>
+		<li><a href="insertactivity.php">Insert an Activity</a></li>
+		<li><a href="deleteactivity.php">Delete an Activity</a></li>
+		<li><a href="updateactivity.php">Update an existing Activity</a></li>
+		<li><a href "listactivity.php">List all existing Activities</a></li>
 	</ul>
 </body>
 </html>
