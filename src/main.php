d09ce3bf5c1c56f4fc9a87d50e2a611891cff527
James Smith <smith7@uleth.ca>
2021-11-11 16:16:50 -0700

    Add text to main.php and added properties.css

--- src/main.php
+++ src/main.php
@@ -1,4 +1,15 @@
+<!doctype html>
 <html>
-  
-  
+<head>
+<meta charset="utf-8">
+<title>Main Page</title>
+<link href="properties.css" rel="stylesheet" type="text/css"</head>
+
+<body>
+	<h1>Events Database</h1>
+	<p>Please select one of the following operations:</p>
+	<ul>
+		<li></li>
+	</ul>
+</body>
 </html>
