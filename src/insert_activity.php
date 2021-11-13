8717d5106739314c5f2f0b03c969bcc98bcbf4ae
Wesley (Wesley) Waldern <walw3660@17enzo.cs.uleth.ca>
2021-11-12 21:52:45 -0700

    adding insert stuff

---
+++ src/insert_activity.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Arenat Ticket - Add New Activity</title></head>
+<body>
+<h2>Add an Activity</h2>
+<form action="insertactivity.php" method=post>
+Activity Name: <input type=text name="Aname" size=30><br><br>
+Activity Date: <input type=text name="date" size=20><br><br>
+Activity Length: <input type=text name="length" size=20><br><br>
+Activity Start Time (2pm = 14:00): <input type=text name="startTime" size=20><br><br>
+<input type=submit name="submit" value="Insert"></form> 
+</body>
+</html>
