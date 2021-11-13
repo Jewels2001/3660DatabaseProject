dcbb35e99d46b5aafb62ffa7463d92070b36a7b4
Jewels2001 <juliewq25@gmail.com>
2021-11-12 22:14:55 -0700

    Update updateactivity.php

--- src/updateactivity.php
+++ src/updateactivity.php
@@ -1,30 +1,29 @@
 <?php
-//if (isset($_COOKIE["username"])) { 
+//if (isset($_COOKIE["username"])) {
 //$username = $_COOKIE["username"];
 //$password = $_COOKIE["password"];
 
 $conn = new mysqli("vconroy.cs.uleth.ca","walw3660","eeb5SaiZuw","walw3660");
-$sql = "update ACTIVITY set Aname='$_POST[Aname]',length='$_POST[length]',startTime='$_POST[startTime]',Adate='$_POST[Adate]' where Aname='$_POST[oldname]'"; 
-if($conn->query($sql)) 
-{ 
+$sql = "update ACTIVITY set Aname='$_POST[Aname]',length='$_POST[length]',startTime='$_POST[startTime]',Adate='$_POST[Adate]' where Aname='$_POST[oldname]'";
+if($conn->query($sql))
+{
 	echo "<h3> Activity updated!</h3>";
 
 } else {
-   $err = $conn->errno(); 
+   $err = $conn->errno();
    if($err == 1062)
    {
-      echo "<p>Activity name $_POST[Aname] already exists!</p>"; 
+      echo "<p>Activity name $_POST[Aname] already exists!</p>";
    } else {
       echo "error code $err";
    }
-   
+
 }
 
 echo "<a href=\"main.php\">Return</a> to Home Page.";
-
-} else {
-   echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>"; 
 }
+// } else {
+//    echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+// }
 //Aname, Adate, length, startTime
 ?>
-
