d114aeea8230a2254a039567d9197e1b19b3bb1e
Wes Waldern <60676132+kingofpixl@users.noreply.github.com>
2021-11-14 00:07:14 -0700

    making changes for site purposes

--- src/main.php
+++ src/main.php
@@ -9,10 +9,10 @@
 	<h1>Events Database</h1>
 	<p>Please select one of the following operations:</p>
 	<ul>
-		<li><a href="insertactivity.php">Insert an Activity</a></li>
-		<li><a href="deleteactivity.php">Delete an Activity</a></li>
-		<li><a href="updateactivity.php">Update an existing Activity</a></li>
-		<li><a href="selectactivity.php">List all existing Activities</a></li>
+		<li><a href="insert_activity.php">Insert an Activity</a></li>
+		<li><a href="delete_activity.php">Delete an Activity</a></li>
+		<li><a href="update_activity.php">Update an existing Activity</a></li>
+		<li><a href="select_activity.php">List all existing Activities</a></li>
 	</ul>
 </body>
 </html>
