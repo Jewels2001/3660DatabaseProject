fd96747b3bed9a24ee09265fd9e37c2f60f6f9e1
James Smith <smith7@uleth.ca>
2021-11-24 17:44:22 -0700

    added delete section

---
+++ src/delete_section.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Arena Ticket - Delete Existing Section</title>
+		<link href="properties.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<h2>Delete a Section</h2>
+		<form action="deletesection.php" method=post>
+			Section Number: <input type=text name="secNumber" size=30><br><br>
+			<input type=submit name="submit" value="Delete">
+		</form>
+	</body>
+</html>
