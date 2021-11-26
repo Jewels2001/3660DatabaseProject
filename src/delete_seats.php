1295bb52153d60c1889607a89a23dea0e8ad9966
ZachaKraus <94146735+ZachaKraus@users.noreply.github.com>
2021-11-26 15:53:57 -0700

    updated naming

--- src/delete_seats.php
+++ src/delete_seats.php
@@ -1,1 +1,13 @@
-
+<html>
+	<head>
+		<title>Arena Ticket - Delete Existing Seat</title>
+		<link href="properties.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<h2>Delete an Activity</h2>
+		<form action="deleteseat.php" method=post>
+			Activity Name: <input type=text name="Aname" size=30><br><br>
+			<input type=submit name="submit" value="Delete">
+		</form>
+	</body>
+</html>
