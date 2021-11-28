c579e2da02d7e4f9b269cf178aee33919531169c
ZachaKraus <94146735+ZachaKraus@users.noreply.github.com>
2021-11-27 21:31:09 -0700

    should be good probably

---
+++ src/select_seats.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Select Arena Ticket</title>
+  </head>
+
+  <body>
+    <h2>List of Seats: </h2>
+    <?php
+      if (isset($_COOKIE["username"])) {
+        echo "<form action=\"selectseats.php\" method=post>";
+
+        $username = $_COOKIE["username"];
+        $password = $_COOKIE["password"];
+
+        $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+
+        $sql = "select ID from SEATS";
+        $result = $conn->query($sql);
+        if($result->num_rows != 0) {
+          echo "Seat ID: <select name=\"ID\">";
+          while($val = $result->fetch_assoc())
+          {
+	           echo "<option value='$val[ID]'>$val[ID]</option>";
+          }
+          echo "</select>";
+          echo "<input type=submit name=\"submit\" value=\"View\">";
+        } else {
+          echo "<p>There are no seats in the system!</p>";
+        }
+        echo "</form>";
+      } else {
+        echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+      }
+    ?>
+  </body>
+</html>
