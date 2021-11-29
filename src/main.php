0bfca8dafd1a86fe4163b32bd2fde4b9a984bec8
Wesley (Wesley) Waldern <walw3660@17enzo.cs.uleth.ca>
2021-11-28 20:44:06 -0700

    updating main

--- src/main.php
+++ src/main.php
@@ -14,6 +14,18 @@
 			<li><a href="delete_activity.php">Delete an Activity</a></li>
 			<li><a href="update_activity.php">Update an existing Activity</a></li>
 			<li><a href="select_activity.php">List all existing Activities</a></li>
+			<li><a href="insert_tixHolder.php">Insert a Ticket Holder</a></li>
+			<li><a href="delete_tixHolder.php">Delete a Ticket Holder</a></li>
+			<li><a href="update_tixHolder.php">Update a existing Ticket Holder</a></li>
+			<li><a href="select_tixHolder.php">List all existing Ticket Holders</a></li>
+			<li><a href="insert_seats.php">Insert a Seat</a></li>
+			<li><a href="delete_seats.php">Delete a Seat</a></li>
+			<li><a href="update_seats.php">Update a existing Seat</a></li>
+			<li><a href="select_seats.php">List all existing seats</a></li>
+			<li><a href="insert_section.php">Insert a Section</a></li>
+			<li><a href="delete_section.php">Delete a Section</a></li>
+			<li><a href="update_section.php">Update a existing Section</a></li>
+			<li><a href="select_section.php">List all existing Sections</a></li>
 		</ul>
 	</body>
 </html>
